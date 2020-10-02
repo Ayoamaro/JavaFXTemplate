e7ae57e0d795350272e1d3a7e85d6a711f14c1fe
Ayoamaro <ayoze.amaro@gmail.com>
2020-10-02 10:47:48 +0100

    Initial commit

---
+++ src/main/java/dad/javafx/template/Main.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package dad.javafx.template;
+
+/**
+ * @author Ayoze Amaro
+ *
+ */
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
